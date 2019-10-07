a527ea48ed042a2ca1153315a456287cdd1d4be9
hika <hika00@gmail.com>
2019-10-07 21:48:57 +0900

    gradle add

--- ein/src/commonMain/kotlin/ein/core/regex/eRegValue.kt
+++ ein/src/commonMain/kotlin/ein/core/regex/eRegValue.kt
@@ -2,51 +2,38 @@ package ein.core.regex
 
 import ein.core.value.*
 
-val regBool = """(true|false)"""
-val regNull = """(null)"""
+val regString = """\"((?:\\"|[^"])*)\"|`([^`]*)`"""
 val regStore = """(?:\@\{([^}]+)\})"""
 val regRecord = """(?:\$\{([^}]*)\})"""
 val regDouble = """(-?(?:0|[1-9]\d*)(?:\.\d+)(?:[eE][-+]?\d+)?(?:dp|%w|%h)?)"""
 val regLong = """(-?(?:0|[1-9]\d*)(?:dp|sp|%w|%h)?)"""
-val regString = """\"((?:\\"|[^"])*)\"|`([^`]*)`"""
-                                //1-bool,2-null,   3-store, 4-record,  5-double,  6-long,  7,8-string
-object eRegValue: eReg("""^\s*(?:$regBool|$regNull|$regStore|$regRecord|$regDouble|$regLong|$regString)\s*"""){
+val regBool = "(true|false)"
+val regNull = "(null)"
+object eRegValue: eReg("""^\s*(?:$regString|$regStore|$regRecord|$regDouble|$regLong|$regBool|$regNull)\s*"""){
     var dp = 1.0
     var sp = 1.0
     var width = 1.0
     var height = 1.0
-    operator fun invoke(v:String) = re.find(v)?.let{
-        val g = it.groups
-        g[1]?.let{eBoolean(it.value.toBoolean())} ?:
-        g[2]?.let{eNull()} ?:
-        g[3]?.let{eStore(it.value)} ?:
-        g[4]?.let{eRecord(it.value)} ?:
-        g[5]?.let{eDouble(dbl(it))} ?:
-        g[6]?.let{eLong(lng(it))} ?:
-        g[7]?.let{eString(it.value.replace("\\\"", "\""))} ?:
-        g[8]?.let{eString(it.value)}
-
+    operator fun invoke(v:String) = re.find(v)?.groups?.let{
+        it[1]?.run{eString(value.replace("\\\"", "\""))} ?:
+        it[2]?.run{eString(value)} ?:
+        it[3]?.run{eStore(value)} ?:
+        it[4]?.run{eRecord(value)} ?:
+        it[5]?.run{eDouble(dbl(this))} ?:
+        it[6]?.run{eLong(lng(this))} ?:
+        it[7]?.run{eBoolean(value.toBoolean())} ?:
+        it[8]?.run{eNull()}
     }
-    fun dbl(it:MatchGroup):Double{
-        val v = it.value
-        return when {
-            v.endsWith("dp") -> v.substring(0, v.length - 2).toDouble()*dp
-            v.endsWith("sp") -> v.substring(0, v.length - 2).toDouble()*sp
-            v.endsWith("%w") -> v.substring(0, v.length - 2).toDouble()* width
-            v.endsWith("%h") -> v.substring(0, v.length - 2).toDouble()* height
-            else -> v.toDouble()
+    fun dbl(it:MatchGroup) = it.value.run{
+        when {
+            endsWith("dp") -> substring(0, length - 2).toDouble()*dp
+            endsWith("sp") -> substring(0, length - 2).toDouble()*sp
+            endsWith("%w") -> substring(0, length - 2).toDouble()* width
+            endsWith("%h") -> substring(0, length - 2).toDouble()* height
+            else -> toDouble()
         }
     }
-    fun lng(it:MatchGroup):Long{
-        val v = it.value
-        return when {
-            v.endsWith("dp")->(v.substring(0, v.length - 2).toDouble()*dp).toLong()
-            v.endsWith("sp")->(v.substring(0, v.length - 2).toDouble()*sp).toLong()
-            v.endsWith("%w")->(v.substring(0, v.length - 2).toDouble()*width).toLong()
-            v.endsWith("%h")->(v.substring(0, v.length - 2).toDouble()*height).toLong()
-            else->v.toLong()
-        }
-    }
+    fun lng(it:MatchGroup) = dbl(it).toLong()
     fun num(it:String):Number?{
         return re.find(it)?.let{
             it.groups[3]?.let{dbl(it)} ?: it.groups[4]?.let{lng(it)}
